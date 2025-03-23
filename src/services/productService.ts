0ed3bbb23a423f6a82ccd6e44546c041132b9ada
vishnupvnhrt <vishnu@nhrtechnologies.com>
2025-03-23 23:38:46 +0530

    api

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // Base URL (optional, if you're using a proxy in React or making calls to the same server)
-const API_URL = "http://13.235.236.219:5000/api/brands";
+const API_URL = "http://13.235.236.219:5000/api/products";
 
 // Fetch all products
 export const getProducts = async () => {

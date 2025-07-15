39f6579c55203a2008fab46165a05de40054b860
ashkar2023 <muhammedashkar2023@gmail.com>
2025-07-15 19:58:48 +0530

    feat: scrollToTop button in Home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,13 +4,15 @@ import { BrandsMarquee } from "../components/landing/BrandsMarquee";
 import { FeaturedProductsSection } from "../components/landing/FeaturedProductsSection";
 import { BannerSection } from "../components/landing/BannerSection";
 import { TestimonialsSection } from "../components/landing/TestimonialsSection";
-import { Zap, Sparkles } from "lucide-react";
+import { Zap, Sparkles, LucideLoaderPinwheel, LucideChevronsUp } from "lucide-react";
 import { VideoSection } from "../components/landing/videoBannerSection";
 import { useEffect, useState } from "react";
 
 
 export const Home = () => {
     const [showForm, setShowForm] = useState(false);
+    const [iframeLoaded, setIframeLoaded] = useState(false);
+    const [showScrollTop, setShowScrollTop] = useState(false);
 
     useEffect(() => {
         const dontShow = localStorage.getItem('dontShowFormAgain');
@@ -24,6 +26,15 @@ export const Home = () => {
         localStorage.setItem('dontShowFormAgain', 'true');
         setShowForm(false);
     };
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setShowScrollTop(window.scrollY > (document.documentElement.scrollHeight / 4));
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
 
     return (
         <main>
@@ -55,18 +66,29 @@ export const Home = () => {
                                 ×
                             </button>
                         </div>
+                        {!iframeLoaded && (
+                            <div className="flex items-center justify-center w-full min-h-[100px]">
+                                <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-red-600"></div>
+                            </div>
+                        )}
                         <iframe
                             src="https://docs.google.com/forms/d/e/1FAIpQLSdsC_CyANtHjv_nfjx9aP4d0EIII1CzOvy2P6mk_7_n2310ug/viewform?embedded=true"
                             width="100%"
                             height="600"
                             className="w-full rounded-xl border border-gray-300 shadow-2xl"
                             title="Contact/Enquiry Form"
+                            onLoad={() => setIframeLoaded(true)}
                         >
-                            Loading…
                         </iframe>
                     </div>
                 </div>
             )}
+            <div
+                className={`fixed z-10 animate-in slide-in-from-bottom right-3 bottom-3 p-1 rounded-md bg-white/60 border border-gray-400 max-h-max w-auto cursor-pointer ${showScrollTop ? "inline": "hidden"}`}
+                onClick={() => window.scrollTo({ behavior: "smooth", top: 0 })}
+            >
+                <LucideChevronsUp />
+            </div>
         </main>
     );
 }

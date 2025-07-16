8c8b166dce5813d6c60a3ebcd182d1bdb59ac593
ashkar2023 <muhammedashkar2023@gmail.com>
2025-07-16 18:51:18 +0530

    fix: scroll products table horizonatlly-admin

--- src/pages/admin/ProductsPage.tsx
+++ src/pages/admin/ProductsPage.tsx
@@ -327,7 +327,7 @@ const ProductsPage = () => {
                         </div>
                     </div>
                 ) : (
-                    <div className="overflow-hidden">
+                    <div className="overflow-x-auto">
                         <table className="min-w-full divide-y divide-gray-200">
                             <thead className="bg-gray-50">
                                 <tr>

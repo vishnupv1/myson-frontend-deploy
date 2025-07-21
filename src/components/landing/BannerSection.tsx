059f7938f576b5861d8ea6cf824b8c3ab642bcc0
ashkar2023 <muhammedashkar2023@gmail.com>
2025-07-21 12:24:22 +0530

    fix: add banner images to assets for serving

--- src/components/landing/BannerSection.tsx
+++ src/components/landing/BannerSection.tsx
@@ -9,7 +9,7 @@ export const BannerSection = () => (
                 <div className="flex-1 min-w-0">
                     <div className="relative w-full aspect-[2/1]">
                         <img
-                            src="https://usa.cateringinsight.com/2019/03/hoshizaki.jpg"
+                            src="/hoshizaki.banner.jpeg"
                             alt="Banner 1"
                             className="absolute inset-0 w-full h-full object-cover rounded-xl shadow-2xl"
                         />
@@ -18,7 +18,7 @@ export const BannerSection = () => (
                 <div className="flex-1 min-w-0">
                     <div className="relative w-full aspect-[2/1]">
                         <img
-                            src="https://scontent.fcok14-1.fna.fbcdn.net/v/t39.30808-6/454376404_1089609396019889_978435876683511908_n.png?stp=dst-png_s960x960&_nc_cat=111&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=JySbCaQ5aHAQ7kNvwHB4W9H&_nc_oc=AdkNqUFwepIg0AlMlFDit9q4pF5xAreMMshHlobh4ipg_lQFdGuNdrNtzjjbUWrxXhU&_nc_zt=23&_nc_ht=scontent.fcok14-1.fna&_nc_gid=CDt7dMqjDuTtqYJcLUsh9g&oh=00_AfR7lJtNq3B2wWAGcSJ2ulSl609eWXH6oP87mNA-eKlHEg&oe=687AAB04"
+                            src="/merrychef.banner.png"
                             alt="Banner 2"
                             className="absolute inset-0 w-full h-full object-cover rounded-xl shadow-2xl"
                         />
